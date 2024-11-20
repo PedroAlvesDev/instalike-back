d518864b67b7b1c2863d72f054f7d97f2d6f049f
Pedro Henrique <139656791+PedroAlvesDev@users.noreply.github.com>
2024-11-20 10:57:28 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import express from "express" ;
+import express from "express"; // Importa o módulo Express para criar a aplicação web.
+import routes from "./src/routes/postsRoutes.js";
 
+// Cria uma instância do Express, que será o núcleo da aplicação.
 const app = express();
+routes(app)
+
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver escutando.
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à imersão!");
-});
