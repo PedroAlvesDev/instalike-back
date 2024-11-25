c89cd7ab75c025cf7b7d09b1def8f10e6da38b89
Pedro Henrique <139656791+PedroAlvesDev@users.noreply.github.com>
2024-11-25 10:35:21 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import routes from "./src/routes/postsRoutes.js";
 
 // Cria uma instância do Express, que será o núcleo da aplicação.
 const app = express();
-routes(app)
+app.use(express.static("uploads"));
+routes(app);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver escutando.
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
-
